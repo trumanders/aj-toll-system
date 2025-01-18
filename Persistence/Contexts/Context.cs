afe5bb2f0fa09c9aefc66bc36ad260837dd817d9
Anders Johansson <trumanders@gmail.com>
2025-01-18 19:37:49 +0100

    add DbContext class and DbSet

---
+++ Persistence/Contexts/Context.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Persistence.Contexts;
+
+public class Context : DbContext
+{
+	public Context(DbContextOptions<Context> options) : base(options) { }
+	public DbSet<TollFee> TollFees { get; set; }
+	public DbSet<TollFeeTimeInterval> TollFeesTimeIntervals { get; set; }
+	public DbSet<TollPassage> TollPassages { get; set; }
+	public DbSet<Vehicle> Vehicles { get; set; }
+	public DbSet<VehicleType> VehicleTypes { get; set; }
+	public DbSet<VehicleOwner> VehicleOwners{ get; set; }
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		base.OnModelCreating(builder);
+
+		// Disable cascade delete here
+
+		// Configure composite keys here (many to many)
+	}
+}
