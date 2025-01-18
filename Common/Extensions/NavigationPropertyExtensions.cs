55aeb0543177ffb2cbdb45e2f44f90cc7927f567
Anders Johansson <trumanders@gmail.com>
2025-01-19 00:16:48 +0100

    Add extension method to check for null navigation property

---
+++ Common/Extensions/NavigationPropertyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Common.Extensions;
+
+public static class NavigationPropertyExtensions
+{
+	public static T EnsureNotNull<T>(this T? property, string propertyName)
+		where T : class
+	{
+		if (property is null)
+		{
+			throw new InvalidOperationException("Uninitialized property: " + propertyName);
+		}
+		return property;
+	}
+}
