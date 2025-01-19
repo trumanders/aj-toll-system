917c227d4e8e069be96834d94f11422ceb0f6dfc
Anders Johansson <trumanders@gmail.com>
2025-01-19 02:52:42 +0100

    Configure swagger and launchSettings.json

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,42 +1,55 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
-namespace Api
+namespace Api;
+
+public class Program
 {
-	public class Program
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+
+		builder.Services.AddControllers();
+		// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerDocument();
+
+		// Register DbService
+		//builder.Services.AddScoped<IDbService, DbService>();
+
+		// Register Context
+		builder.Services.AddDbContext<Context>(options => options.UseSqlServer(
+		builder.Configuration.GetConnectionString("SqlConnection")));
+
+		// Register AutoMapper
+		builder.Services.AddSingleton(new MapperConfiguration(config =>
 		{
-			var builder = WebApplication.CreateBuilder(args);
+			// Map Entity - DTO here
+		}).CreateMapper());
 
-			// Add services to the container.
-
-			builder.Services.AddControllers();
-			// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-			builder.Services.AddOpenApi();
-
-			// Register DbService
-
-			// Register Context
-
-			// Register AutoMapper
+		// Prevent circular references
+		builder.Services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 
 
+		var app = builder.Build();
 
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.MapOpenApi();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseAuthorization();
-
-
-			app.MapControllers();
-
-			app.Run();
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+        {
+			app.UseOpenApi();  // NSwag OpenAPI generation
+			app.UseSwaggerUi();
 		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+
+		app.MapControllers();
+
+		app.Run();
 	}
 }
