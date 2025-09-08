265e658c760df63643de8390ae415d394a1a1123
Anders Johansson <trumanders@gmail.com>
2025-09-08 21:17:18 +0200

    Add DailyFee entity. Mothly fees will be aggregated from DailyFees table.

---
+++ Persistence/Entities/DailyFees.cs
@@ -0,0 +1,17 @@
+﻿namespace Persistence.Entities;
+
+public class DailyFees : IEntity
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	public string PlateNumber { get; set; }
+
+	[Required]
+	public DateTime Date { get; set; }
+
+	[Required]
+	public decimal DailyFee { get; set; }
+}
+
