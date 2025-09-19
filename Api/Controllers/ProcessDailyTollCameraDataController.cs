73936929bd1b9dcad6e603c09e9a2b57488d0114
Anders Johansson <trumanders@gmail.com>
2025-09-20 01:11:27 +0200

    Fix mapping (list)

--- Api/Controllers/ProcessDailyTollCameraDataController.cs
+++ Api/Controllers/ProcessDailyTollCameraDataController.cs
@@ -8,6 +8,6 @@ public class DailyTollCameraDataProcessingController(IMapper _mapper, ITollCamer
 	public async Task<IResult> ProcessDailyTollCameraData(DateTime date, int numberOfPassages)
 	{
 		var dailyFees = await _tollCameraDataProcessingService.ProcessDailyTollCameraData(date, numberOfPassages);
-		return Results.Ok(_mapper.Map<DailyFeeDto>(dailyFees));
+		return Results.Ok(_mapper.Map<List<DailyFeeDto>>(dailyFees));
 	}
 }
